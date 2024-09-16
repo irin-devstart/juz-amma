d8ad8a9ee289ffce9016521f64a05d107e54435e
irin-devstart <irin.devstart@gmail.com>
2024-09-16 15:17:54 +0700

    Fisihing code

---
+++ index.js
@@ -0,0 +1,15 @@
+// Shared element
+export { default as SharedElement } from './components/SharedElement';
+export { default as SharedElementRenderer } from './components/SharedElementRenderer';
+// Attentions
+export { default as Shake } from './components/Shake';
+// Available Animations
+export { default as AnimatedNumber } from './components/AnimatedNumber';
+export { default as AnimatedText } from './components/AnimatedText';
+export { default as ScaleAndOpacity } from './components/ScaleAndOpacity';
+export { default as Scale } from './components/Scale';
+export { default as TranslateYAndOpacity } from './components/TranslateYAndOpacity';
+export { default as TranslateY } from './components/TranslateY';
+export { default as TranslateX } from './components/TranslateX';
+export { default as TranslateXY } from './components/TranslateXY';
+export { default as Opacity } from './components/Opacity';
